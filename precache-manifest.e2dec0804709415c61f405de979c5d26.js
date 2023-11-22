efeeff3eadfbf0b7bfd0af87a705f87b3ca909c6
mkim45 <michaelkim2025.1@u.northwestern.edu>
2023-11-22 17:16:09 -0600

    Updates

---
+++ precache-manifest.e2dec0804709415c61f405de979c5d26.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e9e0e1db1373f531e33",
+    "url": "/static/css/main.129d0f09.chunk.css"
+  },
+  {
+    "revision": "6e9e0e1db1373f531e33",
+    "url": "/static/js/main.8821f4af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f66d47b00f13af4379b",
+    "url": "/static/js/2.3de31666.chunk.js"
+  },
+  {
+    "revision": "bef85804c4746b57c9e4c735b9244b41",
+    "url": "/static/media/GitHub.bef85804.png"
+  },
+  {
+    "revision": "8e0a3e1bf83e10442b9489a0addbdc4f",
+    "url": "/static/media/Battery.8e0a3e1b.png"
+  },
+  {
+    "revision": "82e204cbd4444cd03e2859f155b3f0c9",
+    "url": "/static/media/Proteins1.82e204cb.png"
+  },
+  {
+    "revision": "1a3765dfbb73241c792ad1df81917e44",
+    "url": "/static/media/Proteins2.1a3765df.png"
+  },
+  {
+    "revision": "7f6e552df6a5532af79222769c9b58f2",
+    "url": "/static/media/Proteins3.7f6e552d.png"
+  },
+  {
+    "revision": "60ecaed90218e536ad6992519202dbec",
+    "url": "/static/media/Proteins4.60ecaed9.png"
+  },
+  {
+    "revision": "fd8e6d027825ed1e8e2bd90760cd094e",
+    "url": "/static/media/Proteins5.fd8e6d02.png"
+  },
+  {
+    "revision": "f0cedc30e1f02a3da61ffc1407596474",
+    "url": "/static/media/Proteomics.f0cedc30.jpg"
+  },
+  {
+    "revision": "281258cc6fccafb533ad356617160fed",
+    "url": "/static/media/BatteryWork.281258cc.png"
+  },
+  {
+    "revision": "7a0eedace66b75418cc0b7adc839aaf3",
+    "url": "/static/media/Inbound.7a0eedac.png"
+  },
+  {
+    "revision": "d48717aabd65c17f7b6031159e8b2941",
+    "url": "/static/media/NorthwesternWork.d48717aa.jpg"
+  },
+  {
+    "revision": "aab05ab85114ecad4929f5b765bd0892",
+    "url": "/static/media/Michael_Kim.aab05ab8.png"
+  },
+  {
+    "revision": "3d70d08a3a16db19aea425e38d7edbaa",
+    "url": "/static/media/LinkedInLogo.3d70d08a.png"
+  },
+  {
+    "revision": "851c06c945f31571c69d593681cfec54",
+    "url": "/static/media/BatteryTool.851c06c9.png"
+  },
+  {
+    "revision": "e8dfc23d1eac1953083ca8a999862471",
+    "url": "/static/media/I2MSComparison.e8dfc23d.png"
+  },
+  {
+    "revision": "28214d0f5cbdc89c9e1008d98b99333e",
+    "url": "/static/media/AboutMe.28214d0f.png"
+  },
+  {
+    "revision": "60f6fc33138030043bbe76986f9c49cd",
+    "url": "/static/media/Science1.60f6fc33.png"
+  },
+  {
+    "revision": "6424a776855d38fff510d1050d03ba43",
+    "url": "/static/media/Uber.6424a776.png"
+  },
+  {
+    "revision": "beb2eeef0a335436f274bcea90bd12ad",
+    "url": "/static/media/Wine.beb2eeef.png"
+  },
+  {
+    "revision": "2f02d989656f5a68ed790355faaff2bf",
+    "url": "/static/media/Yelp.2f02d989.png"
+  },
+  {
+    "revision": "532b247ce50079129966b02a8e8d12c5",
+    "url": "/static/media/Chess.532b247c.png"
+  },
+  {
+    "revision": "e454e8d6119040976d80534f086a8d2c",
+    "url": "/static/media/SciOly.e454e8d6.png"
+  },
+  {
+    "revision": "3ea87036ac878d982a3162eefd8b367f",
+    "url": "/static/media/NET.3ea87036.png"
+  },
+  {
+    "revision": "bd2a1a07238973548851d1ce254f6d79",
+    "url": "/static/media/Azure.bd2a1a07.png"
+  },
+  {
+    "revision": "e9c80f65cad3f9afa7c3a26e51fb4299",
+    "url": "/static/media/Blazor.e9c80f65.png"
+  },
+  {
+    "revision": "f9a8e13d05918b3e91ede5be0f993956",
+    "url": "/static/media/C.f9a8e13d.png"
+  },
+  {
+    "revision": "702a8adb221c409b25caa74b9f187bd6",
+    "url": "/static/media/CSharp.702a8adb.png"
+  },
+  {
+    "revision": "d3a7dcfa81d16c7926f0827f086da263",
+    "url": "/static/media/C++.d3a7dcfa.png"
+  },
+  {
+    "revision": "fa3861ca389b3a7826fb8e011b0d421d",
+    "url": "/static/media/CSS.fa3861ca.png"
+  },
+  {
+    "revision": "356ed733ae3475e47d9421eb750e4103",
+    "url": "/static/media/FSharp.356ed733.png"
+  },
+  {
+    "revision": "f33ea693bc7f1a4ace412290b8d8a9d9",
+    "url": "/static/media/Figma.f33ea693.png"
+  },
+  {
+    "revision": "3e3456b5fa2a90e8cd8a99ebd1b499f1",
+    "url": "/static/media/Firebase.3e3456b5.png"
+  },
+  {
+    "revision": "894fd61ae842dc756c47c0fa90d56669",
+    "url": "/static/media/Git.894fd61a.png"
+  },
+  {
+    "revision": "d92d38d16ddb69e6b023d71ab53e8233",
+    "url": "/static/media/HTML.d92d38d1.png"
+  },
+  {
+    "revision": "f41c52940a763f4852304b8ed3da0f58",
+    "url": "/static/media/Hugging.f41c5294.png"
+  },
+  {
+    "revision": "c972f922579a2df66efac2bb4bb966f5",
+    "url": "/static/media/Java.c972f922.png"
+  },
+  {
+    "revision": "8829423e34a26a0b4b0d9fb31d3c2bcc",
+    "url": "/static/media/Javascript.8829423e.png"
+  },
+  {
+    "revision": "073985e7777c510ff6e09b0b451884c3",
+    "url": "/static/media/Keras.073985e7.png"
+  },
+  {
+    "revision": "f6823223e6aef5705e7f0129ca2d0cf2",
+    "url": "/static/media/Matplotlib.f6823223.png"
+  },
+  {
+    "revision": "89f11bf146d0121fb0814670ab2e42d1",
+    "url": "/static/media/MySQL.89f11bf1.png"
+  },
+  {
+    "revision": "3c6f7b5b3df9ca3b8f54bd55c17eab21",
+    "url": "/static/media/Node.3c6f7b5b.png"
+  },
+  {
+    "revision": "78e86386ef42c2ec8b20f301025563f7",
+    "url": "/static/media/Npm.78e86386.png"
+  },
+  {
+    "revision": "6f1aaf6783bcf7cd7cec40cc1f1af1c2",
+    "url": "/static/media/Numpy.6f1aaf67.png"
+  },
+  {
+    "revision": "1cad590e80847e082d4d39cc7e1575b3",
+    "url": "/static/media/Pandas.1cad590e.png"
+  },
+  {
+    "revision": "326bea94b7bb7b412583af7c33d01f31",
+    "url": "/static/media/Postgre.326bea94.png"
+  },
+  {
+    "revision": "f1f0989aa37bc76485285998316e98b4",
+    "url": "/static/media/Python.f1f0989a.png"
+  },
+  {
+    "revision": "f75367ca6403fe6d4a6fcdbc968cca10",
+    "url": "/static/media/Pytorch.f75367ca.png"
+  },
+  {
+    "revision": "9498e3baf8b71ff8b4ab56726611b12c",
+    "url": "/static/media/R.9498e3ba.png"
+  },
+  {
+    "revision": "355bd61fe4f640cb4fd28e8b409cdb14",
+    "url": "/static/media/Racket.355bd61f.png"
+  },
+  {
+    "revision": "576a5775e4fdd3fae6b5677feabecf3c",
+    "url": "/static/media/React.576a5775.png"
+  },
+  {
+    "revision": "41030b4088548cc529411cc1ca0b94c2",
+    "url": "/static/media/SAS.41030b40.png"
+  },
+  {
+    "revision": "cb9ec9e6b9ca47a8c869cf5bbbfa7364",
+    "url": "/static/media/Scikit.cb9ec9e6.png"
+  },
+  {
+    "revision": "7b28b021b851915f5c451f9c7f053d88",
+    "url": "/static/media/Scipy.7b28b021.png"
+  },
+  {
+    "revision": "4c6cfe5cc2a6fe5474ec28e9548066ab",
+    "url": "/static/media/Seaborn.4c6cfe5c.png"
+  },
+  {
+    "revision": "df5d0be0618b650b99b74da514eed205",
+    "url": "/static/media/SQLite.df5d0be0.png"
+  },
+  {
+    "revision": "f7016674dad1f60c199f254b12507e54",
+    "url": "/static/media/Statsmodels.f7016674.png"
+  },
+  {
+    "revision": "112fa78c278b4445e2058d8ce7454286",
+    "url": "/static/media/Tensorflow.112fa78c.png"
+  },
+  {
+    "revision": "4d605e188e0ff103f5a48cf6b59747e4",
+    "url": "/static/media/WebAssembly.4d605e18.png"
+  },
+  {
+    "revision": "86aaf44c94fd2beae9ee36afa641c7ef",
+    "url": "/index.html"
+  }
+];
